76ae2f902102ea2987f5a5dbe1b68bc8079e85e9
Harsh Mungara <harsh.mungara2004@gmail.com>
2022-01-23 22:09:18 +0300

    commiting

--- tink2.py
+++ tink2.py
@@ -3,7 +3,10 @@ from tkinter import *
 root = Tk()
 
 mylabel = Label(root, text="Hello World!")
+mylabel2= Label(root,text="We are just learning grid").grid(row=4,column=5)
 
-mylabel.pack()
+mylabel.grid(row=0,column=1)
+
+'''mylabel.pack()'''
 
 root.mainloop()
